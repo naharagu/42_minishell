2a188ee1b277c883b72e29a2f0a555a2276af7f5
naharagu <76852123+naharagu@users.noreply.github.com>
2022-12-31 15:43:49 +0100

    added libft

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,13 +6,13 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 06:19:05 by naharagu          #+#    #+#             */
-/*   Updated: 2022/11/03 22:21:10 by naharagu         ###   ########.fr       */
+/*   Updated: 2022/07/11 05:40:59 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(int))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst || !del)
 		return ;
--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,13 +6,13 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 06:19:05 by naharagu          #+#    #+#             */
-/*   Updated: 2022/11/03 22:23:26 by naharagu         ###   ########.fr       */
+/*   Updated: 2022/07/14 23:25:32 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(int))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!f)
 		return ;
