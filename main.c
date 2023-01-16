87127c70aa48fd52f5410bc5cf19445fb4131329
naharagu <76852123+naharagu@users.noreply.github.com>
2023-01-16 22:13:43 +0100

    step3 exec path

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 22:47:25 by naharagu          #+#    #+#             */
-/*   Updated: 2023/01/16 20:57:57 by naharagu         ###   ########.fr       */
+/*   Updated: 2023/01/16 22:12:37 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,24 @@ void	init_struct_ms(t_minishell *ms)
 	return ;
 }
 
-void	minishell(t_minishell *ms)
+int	execute_cmd(char *input, char **argv, char **env)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		execve(input, argv, env);
+	}
+	else
+	{
+		wait(&status);
+		return (status);
+	}
+}
+
+void	minishell(t_minishell *ms, char **argv, char **env)
 {
 	char	*input;
 
@@ -31,10 +48,9 @@ void	minishell(t_minishell *ms)
 		if (!input)
 			break ;
 		add_history(input);
-		printf("%s\n", input);
+		execute_cmd(input, argv, env);
 		free(input);
 	}
-	exit(ms->exit_status);
 }
 
 int	main(int argc, char **argv, char **env)
@@ -42,6 +58,6 @@ int	main(int argc, char **argv, char **env)
 	t_minishell	*ms;
 
 	init_struct_ms(ms);
-	minishell(ms);
-	return (0);
+	minishell(ms, argv, env);
+	exit(0);
 }
