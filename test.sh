dbe35b3443b379b5f9b035c68c83451bdaed85aa
naharagu <76852123+naharagu@users.noreply.github.com>
2023-01-16 21:48:46 +0100

    step2. test

---
+++ test.sh
@@ -0,0 +1,20 @@
+assert() {
+	printf '%-30s:' "\"$1\""
+	# exit status
+	echo -n -e "$1" | bash >cmp 2>&-
+	expected=$?
+	echo -n -e "$1" | ./minishell >out 2>&-
+	actual=$?
+
+	diff cmp out >/dev/null && echo -n '  diff OK' || echo -n '  diff NG'
+
+	if [ "$actual" = "$expected" ]; then
+		echo -n '  status OK'
+	else
+		echo -n "  status NG, expected $expected but got $actual"
+	fi
+	echo
+}
+
+# Empty line (EOF)
+assert ''
