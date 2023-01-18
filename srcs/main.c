c53f6bbea0765dce99d0e3e2fa13cbed7a257d2a
kaorishima <mk.kaorishima@gmail.com>
2023-01-18 23:09:45 +0900

    split clear too many lines

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 16:53:39 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/01/18 14:39:39 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/01/18 23:08:34 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,54 @@ void	minishell(t_minishell *ms)
 	exit(ms->exit_status);
 }
 
-t_mslist	*add_list(t_minishell *ms, t_mslist	*list)
+// void	*add_list(t_minishell *ms, t_mslist	**list)
+// {
+// 	t_mslist	*tmp;
+// 	size_t		len;
+
+// 	while (*ms->input)
+// 	{
+// 		len = 0;
+// 		while (*ms->input && is_quoted(*ms->input, ms))
+// 		{
+// 			ms->input++;
+// 			len++;
+// 		}
+// 		while (*ms->input && !(is_space(*ms->input)) \
+// 			&& !(is_delimiter(*ms->input)))
+// 		{
+// 			ms->input++;
+// 			len++;
+// 		}
+// 		if (len > 0)
+// 		{
+// 			tmp = ms_lstnew(len, ms->input - len);
+// 			ms_lstadd_back(list, tmp);
+// 		}
+// 		if (is_delimiter(*ms->input))
+// 		{
+// 			len = 0;
+// 			while (is_delimiter(*ms->input))
+// 			{
+// 				ms->input++;
+// 				len++;
+// 			}
+// 			tmp = ms_lstnew(len, ms->input - len);
+// 			ms_lstadd_back(list, tmp);
+// 			ms->input--;
+// 		}
+// 		ms->input++;
+// 	}
+// }
+
+void	split_token(t_minishell *ms)
 {
+	t_mslist	*list;
 	t_mslist	*tmp;
 	size_t		len;
+	size_t		i;//
 
+	list = NULL;
 	while (*ms->input)
 	{
 		len = 0;
@@ -75,16 +118,6 @@ t_mslist	*add_list(t_minishell *ms, t_mslist	*list)
 		}
 		ms->input++;
 	}
-	return (list);
-}
-
-void	split_token(t_minishell *ms)
-{
-	t_mslist	*list;
-	size_t		i;//
-
-	list = NULL;
-	list = add_list(ms, list);
 	i = 0;//
 	while (i < ms_lstsize(list))//
 	{//
