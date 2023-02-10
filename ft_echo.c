923272566c194266fe249d9f117950763abbed70
naharagu <76852123+naharagu@users.noreply.github.com>
2023-02-10 07:47:54 +0100

    add echo, pwd

--- ft_echo.c
+++ ft_echo.c
@@ -6,14 +6,14 @@
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 22:40:44 by naharagu          #+#    #+#             */
-/*   Updated: 2023/01/28 23:15:43 by naharagu         ###   ########.fr       */
+/*   Updated: 2023/02/05 22:13:47 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <stdbool.h>
 
-static bool is_consective_n(char *s)
+static bool	is_all_n(char *s)
 {
 	size_t	i;
 
@@ -29,30 +29,48 @@ static bool is_consective_n(char *s)
 	return (true);
 }
 
+static bool	skip_n_option(char **argv, size_t *i)
+{
+	bool	n_option;
+
+	n_option = false;
+	while (argv[*i] && *(argv[*i] == '-'))
+	{
+		if (!is_all_n(argv[*i]))
+			break ;
+		n_option = true;
+		(*i)++;
+	}
+	return (n_option);
+}
+
 int	ft_echo(int argc, char **argv)
 {
-	bool option_n;
-	size_t i;
+	size_t	i;
+	bool	n_option;
 
 	i = 1;
-	option_n = is_consective_n(argv[1]);
 	while (i < argc)
 	{
+		n_option = skip_n_option(argv, &i);
 		ft_putstr_fd(argv[i], 1);
 		if (i != argc - 1)
 			write(1, " ", 1);
 		i++;
 	}
-	if (!is_consective_n(argv[1]))
+	if (!n_option)
 		write(1, "\n", 1);
 	return (EXIT_SUCCESS);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
 		ft_echo(argc, argv);
 	}
-	return 0;
+	return (0);
 }
+
+//echo -n -n -nnn hello
+// > hello
--- ft_pwd.c
+++ ft_pwd.c
@@ -1,15 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_unset.c                                         :+:      :+:    :+:   */
+/*   ft_pwd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: naharagu <naharagu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/28 22:22:08 by naharagu          #+#    #+#             */
-/*   Updated: 2023/01/28 22:56:42 by naharagu         ###   ########.fr       */
+/*   Created: 2023/01/28 22:40:44 by naharagu          #+#    #+#             */
+/*   Updated: 2023/02/10 07:42:46 by naharagu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <stdbool.h>
 
-// static bool is_valid()
+int	ft_pwd(int argc, char **argv)
+{
+	char *pwd;
+
+	pwd = getcwd(NULL, 0);
+	if (!pwd)
+		return (EXIT_FAILURE);
+	else
+		ft_putstr_fd(pwd, 1);
+	ft_putchar_fd('\n', 1);
+	return (EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv)
+{
+	ft_pwd(argc, argv);
+	return 0;
+}
