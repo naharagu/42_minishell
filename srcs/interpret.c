c519321819701b293fdcbfa2462f437a63a3d472
kaorishima <mk.kaorishima@gmail.com>
2023-02-25 22:13:45 +0900

    fixed interpret

--- srcs/interpret.c
+++ srcs/interpret.c
@@ -6,33 +6,46 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 16:59:50 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/02/25 17:20:04 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/02/25 22:13:00 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	free_error(t_minishell *ms, char **argv, char *str);
+
 int	interpret(t_minishell *ms, char *line)
 {
 	extern char	**environ;
-	char		*argv[] = {line, NULL};
+	char		**argv;
 	pid_t		pid;
 	int			wstatus;
 
 	pid = fork();
+	argv = ft_calloc(2, sizeof(char *));
+	if (!argv)
+		print_error(ms, "malloc", EXIT_ERR);
+	argv[0] = ft_strdup(line);
+	argv[1] = NULL;
 	if (pid < 0)
-		print_error(ms, "fork", EXIT_ERR);
+		free_error(ms, argv, "fork");
 	else if (pid == 0)
 	{
-		// child process
-		execve(line, argv, environ);
-		print_error(ms, "execve", EXIT_ERR);
+		if (execve(line, argv, environ))
+			free_error(ms, argv, "execve");
 	}
-	else
+	else if (pid > 0)
 	{
-		// parent process
 		wait(&wstatus);
 		return (WEXITSTATUS(wstatus));
 	}
+	free_error(ms, argv, "");
 	return (0);
+}
+
+void	free_error(t_minishell *ms, char **argv, char *str)
+{
+	free(argv[0]);
+	free(argv);
+	print_error(ms, str, EXIT_ERR);
 }
