c9f7d6c92f232ef278f03ab9a0388ebda93e8f63
kaorishima <mk.kaorishima@gmail.com>
2023-02-27 22:05:45 +0900

    fixed interpret

--- srcs/interpret.c
+++ srcs/interpret.c
@@ -6,20 +6,19 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 16:59:50 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/02/27 17:01:50 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/02/27 22:05:11 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	child_process(t_minishell *ms, char **argv);
 char	*serch_path(char *file);
 void	free_argv_error(t_minishell *ms, char **argv, char *str);
 
 int	interpret(t_minishell *ms)
 {
-	extern char	**environ;
 	char		**argv;
-	char		*path;
 	pid_t		pid;
 	int			wstatus;
 
@@ -29,26 +28,10 @@ int	interpret(t_minishell *ms)
 		print_error(ms, "malloc", EXIT_ERR);
 	argv[0] = ft_strdup(ms->line);
 	argv[1] = NULL;
-	path = argv[0];
 	if (pid < 0)
 		free_argv_error(ms, argv, "fork");
 	else if (pid == 0)
-	{
-		if (execve(ms->line, argv, environ))
-			free_argv_error(ms, argv, "execve");
-		if (!(ft_strchr(path, '/')))
-		{
-			path = serch_path(path);
-			if (!path || access(path, F_OK) < 0)
-			{
-				ms->exit_status = 127;
-				ms->err_str = ft_strjoin(argv[0], ": command not found");
-				print_error(ms, ms->err_str, OTHER_ERR);
-			}
-			if (execve(path, argv, environ))
-				free_argv_error(ms, argv, "execve");
-		}
-	}
+		child_process(ms, argv);
 	else if (pid > 0)
 	{
 		wait(&wstatus);
@@ -58,29 +41,45 @@ int	interpret(t_minishell *ms)
 	return (0);
 }
 
+void	child_process(t_minishell *ms, char **argv)
+{
+	extern char	**environ;
+	char		*path;
+
+	path = argv[0];
+	if (!(ft_strchr(path, '/')))
+	{
+		path = serch_path(path);
+		if (!path || access(path, F_OK) < 0)
+		{
+			ms->exit_status = 127;
+			ms->err_str = ft_strjoin(argv[0], ": command not found");
+			print_error(ms, ms->err_str, OTHER_ERR);
+		}
+		if (execve(path, argv, environ))
+			free_argv_error(ms, argv, "execve");
+	}
+	else if (execve(ms->line, argv, environ))
+		free_argv_error(ms, argv, "execve");
+}
+
 char	*serch_path(char *file)
 {
-	char	*path;
+	char	**path;
 	char	*value;
-	char	*end;
+	size_t	i;
 
 	value = getenv("PATH");
-	printf("value= %s\n", value);//
-	while (*value)
+	path = ft_split(value, ':');
+	i = 0;
+	while (path[i])
 	{
-		end = ft_strchr(value, ':');
-		if (end)
-			ft_strlcpy(path, value, end - value);
-		else
-			ft_strlcpy(path, value, ft_strlen(value));
-		ft_strlcat(path, "/", ft_strlen(path));
-		ft_strlcat(path, file, ft_strlen(path));
-		printf("path= %s\n", path);//
-		if (!(access(path, X_OK)))
-			return (path);
-		// if (!end)
-		// 	return (NULL);
-		value = end + 1;
+		path[i] = ft_strjoin(path[i], "/");
+		path[i] = ft_strjoin(path[i], file);
+		//printf("path= %s\n", path[i]);//
+		if (!(access(path[i], X_OK)))
+			return (path[i]);
+		i++;
 	}
 	return (NULL);
 }
