a777d327f8740c127f92e1797246f2c2620ec661
kaorishima <mk.kaorishima@gmail.com>
2023-03-01 15:08:50 +0900

    fixed list

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,13 +6,14 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 09:59:13 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/03/01 13:07:59 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/03/01 15:07:35 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static char	*add_mslist(char *line, t_mslist	**list);
+//static char	*add_mslist(char *line, t_mslist **list);
+void	add_mslist(t_minishell *ms, char *start, char *line, size_t len);
 
 void	lexer(t_minishell *ms)
 {
@@ -30,28 +31,40 @@ void	lexer(t_minishell *ms)
 			!(is_delimiter(*ms->line)))
 			ms->line++;
 		len = ms->line - start;
+		printf ("len= %zu\n", len);//
 		if (len > 0)
 		{
 			tmp = ms_lstnew(len, start);
 			ms_lstadd_back(&ms->list, tmp);
 		}
+		//add_mslist(ms, start, ms->line, len);
 		start = ms->line;
 		if (is_delimiter(*ms->line))
 		{
 			while (*ms->line && (is_delimiter(*ms->line) || is_metachara(*ms->line)))
 				ms->line++;
 			len = ms->line - start;
-			printf("len=  %zu\n", len);//
+			printf ("len= %zu\n", len);//
 			tmp = ms_lstnew(len, start);
-			printf("tmpstr2= %s\n", tmp->str);//
 			ms_lstadd_back(&ms->list, tmp);
 		}
+		//add_mslist(ms, start, ms->line, len);
 		while (*ms->line && is_space(*ms->line))
 			ms->line++;
-		if (!(ft_strlen(ms->line)))
-			break ;
 	}
 	error_lexer(ms);
+}
+
+void	add_mslist(t_minishell *ms, char *start, char *line, size_t len)
+{
+	t_mslist	*tmp;
+
+	printf ("len= %zu\n", len);//
+	if (len > 0)
+	{
+		tmp = ms_lstnew(len, start);
+		ms_lstadd_back(&ms->list, tmp);
+	}
 }
 
 // static char	*add_mslist(char *line, t_mslist **list)
