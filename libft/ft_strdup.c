48849673bfd23bb77675194410ff42638cee782f
kaorishima <mk.kaorishima@gmail.com>
2023-03-13 16:09:30 +0900

    changed ft_strdup

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,21 +6,21 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/29 16:13:20 by naharagu          #+#    #+#             */
-/*   Updated: 2023/02/07 21:48:59 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/03/13 16:07:33 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s)
 {
-	size_t	len;
-	char	*dest;
+	char		*addr;
+	size_t		len;
 
-	len = ft_strlen(s1);
-	dest = malloc((len + 1) * sizeof(char));
-	if (!dest)
+	len = ft_strlen(s);
+	addr = (char *)ft_calloc((len + 1), sizeof(char));
+	if (!addr)
 		return (NULL);
-	ft_strlcpy(dest, s1, len + 1);
-	return (dest);
+	ft_memcpy(addr, s, len);
+	return (addr);
 }
