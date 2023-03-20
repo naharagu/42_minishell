78c599b2988a6999a6a45d425d23d2a2ab1d3d1b
kaorishima <mk.kaorishima@gmail.com>
2023-03-20 16:36:00 +0900

    ft_cd.c clear

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/01/12 11:05:34 by shimakaori        #+#    #+#              #
-#    Updated: 2023/03/20 15:55:03 by shimakaori       ###   ########.fr        #
+#    Updated: 2023/03/20 16:34:58 by shimakaori       ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
--- srcs/ft_cd.c
+++ srcs/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 15:54:26 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/03/20 16:07:14 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/03/20 16:35:33 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ void	ft_cd(t_minishell *ms, char *newdir)
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 		exit_error(ms, "getcwd");
-	printf("oldcwd= %s\n", cwd);//
 	if (chdir(newdir) < 0)
-		exit_error(ms, "chdir");
+		other_error(ms, "cd", ft_strjoin(newdir, \
+		": No such file or directory"), 1);
 	cwd = getcwd(NULL, 0);
-	printf("newcwd= %s\n", cwd);//
 }
