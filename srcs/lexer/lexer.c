5dd5f7e078f31dd57791cf205715e62fbb495632
kaorishima <mk.kaorishima@gmail.com>
2023-04-05 20:02:44 +0900

    delete $ in lexer

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 09:59:13 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/04/05 19:33:02 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/04/05 20:01:28 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,7 @@ void	lexer(t_minishell *ms)
 	while (*ms->line)
 	{
 		start = ms->line;
-		while (*ms->line && *ms->line == '$')
-			ms->line++;
-		while (*ms->line && *ms->line != '$' && !(is_space(*ms->line)) \
+		while (*ms->line && !(is_space(*ms->line)) \
 		&& !(is_metachara(*ms->line)) && *ms->line != '\'' && *ms->line != '\"')
 			ms->line++;
 		while (*ms->line && is_quoted(*ms->line, ms))
