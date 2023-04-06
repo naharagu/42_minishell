399bc62ed7c2500464376090c25d78330001cb95
kaorishima <mk.kaorishima@gmail.com>
2023-04-06 20:41:30 +0900

    update lexer

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 09:59:13 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/04/06 20:34:23 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/04/06 20:35:04 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	lexer(t_minishell *ms)
 		&& !(is_metachara(*ms->line)) && *ms->line != '\'' && *ms->line != '\"')
 			ms->line++;
 		add_mslist(ms, start, ms->line);
-		start = ms->line;		
+		start = ms->line;
 		while (*ms->line && ft_isdigit(*ms->line))
 			ms->line++;
 		while (*ms->line && is_metachara(*ms->line))
