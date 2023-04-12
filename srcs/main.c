8f4e9ca117cd29a29f5200916f01b52d6b99316c
kaorishima <mk.kaorishima@gmail.com>
2023-04-12 09:02:20 +0900

    before submmit

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: shimakaori <shimakaori@student.42tokyo.jp> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 16:53:39 by shimakaori        #+#    #+#             */
-/*   Updated: 2023/04/11 10:05:27 by shimakaori       ###   ########.fr       */
+/*   Updated: 2023/04/11 10:34:23 by shimakaori       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ static void	prompt_helper(t_minishell *ms)
 {
 	if (lexer(ms) == EXIT_FAILURE)
 		return ;
-	print_mslist(ms);//
+	//print_mslist(ms);//
 	if (parser(ms) == EXIT_FAILURE)
 		return ;
 	// print_cmdline(ms);
